2c953851aef3d7d5eee1dfe2f426a301e3837276
linn <linncoffeee@gmail.com>
2023-08-04 22:56:57 +0630

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('Staff');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'stuff'])->default('admin');
+            $table->enum('role', ['admin', 'staff'])->default('staff');
             $table->string("photo")->default(config("info.default_user_photo"));
             $table->rememberToken();
             $table->timestamps();
