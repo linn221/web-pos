03ac30f49956e1e4250306f2d7ba357908477281
MyoMinnNaing <myominnaing.webdev@gmail.com>
2023-08-07 16:20:38 +0630

    update dev

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function() {
-    Route::middleware("auth:sanctum")->group(function() {
+Route::prefix('v1')->group(function () {
+    Route::middleware("auth:sanctum")->group(function () {
         Route::get('/logout', [ApiAuthController::class, 'logout']);
         Route::post("/logout-all", [ApiAuthController::class, 'logoutAll']);
         Route::get("/tokens", [ApiAuthController::class, 'tokens']);
