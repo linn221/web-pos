1ce6c918a815b3e2a9fec9fb8b8f4ec7cfacc6aa
linn <linncoffeee@gmail.com>
2023-08-08 11:17:15 +0630

    move code for updating total_stock to observer

---
+++ app/Observers/VoucherRecordObserver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use App\Models\VoucherRecord;
+
+class VoucherRecordObserver
+{
+    /**
+     * Handle the VoucherRecord "created" event.
+     */
+    public function created(VoucherRecord $voucherRecord): void
+    {
+        $voucherRecord->product()->increment('total_stock', $voucherRecord->quantity);
+        //
+    }
+
+    /**
+     * Handle the VoucherRecord "updated" event.
+     */
+    public function updating(VoucherRecord $voucherRecord): void
+    {
+        // $voucherRecord->product->decrement($voucherRecord->quantity);
+        //
+    }
+    public function updated(VoucherRecord $voucherRecord): void
+    {
+        // $voucherRecord->product->increment($voucherRecord->quantity);
+        //
+    }
+
+    /**
+     * Handle the VoucherRecord "deleted" event.
+     */
+    public function deleting(VoucherRecord $voucherRecord): void
+    {
+        $voucherRecord->product()->decrement('total_stock', $voucherRecord->quantity);
+        //
+    }
+}
