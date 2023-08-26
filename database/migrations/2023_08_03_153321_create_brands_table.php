34f23812753b71b7bc7d1bd0141785433c53b288
linn <linncoffeee@gmail.com>
2023-08-26 18:03:58 +0630

    update migration for photo column

--- database/migrations/2023_08_03_153321_create_brands_table.php
+++ database/migrations/2023_08_03_153321_create_brands_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('photo')->nullable();
             $table->string('company');
             $table->string('agent');
             $table->text('description')->nullable();
